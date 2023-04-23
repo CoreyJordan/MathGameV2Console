8dba72b2174ff1ca159fdbaf88deea6b2a9c9b94
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-04-23 11:15:47 -0400

    feat Program: Add WelcomeUser()

--- MathGameV2/Program.cs
+++ MathGameV2/Program.cs
@@ -1,4 +1,11 @@
 ﻿
+WelcomeUser();
 
-
-
+static void WelcomeUser()
+{
+    Console.Clear();
+    Console.WriteLine();
+    Console.WriteLine("Welcome to Math Game v2");
+    Console.WriteLine("Designed and developed by Corey Jordan, 2023");
+    Console.WriteLine();
+}
