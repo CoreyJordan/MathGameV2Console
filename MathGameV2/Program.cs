440b59e2192ccdc8e74958c3180df21e04de146c
Corey Jordan <113132164+CoreyJordan@user.noreply.github.com>
2023-04-23 11:38:45 -0400

    feat Program: Add GetPlayerName() to create player

--- MathGameV2/Program.cs
+++ MathGameV2/Program.cs
@@ -1,5 +1,12 @@
-﻿
+﻿using MathGameLibrary.Player;
+
 WelcomeUser();
+Player player = new()
+{
+    PlayerName = GetPlayerName()
+};
+
+
 
 static void WelcomeUser()
 {
@@ -8,4 +15,21 @@ static void WelcomeUser()
     Console.WriteLine("Welcome to Math Game v2");
     Console.WriteLine("Designed and developed by Corey Jordan, 2023");
     Console.WriteLine();
+}
+
+static string GetPlayerName()
+{
+    string name = string.Empty;
+    Console.Write("Please enter your name, player: ");
+
+    while (name == string.Empty)
+    {
+        name = Console.ReadLine()!;
+        if (name == string.Empty)
+        {
+            Console.WriteLine("I'm sorry, I didn't quite catch that.");
+            Console.Write("PLease eneter your name: ");
+        }
+    }
+    return name;
 }
